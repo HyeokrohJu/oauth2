3b78078583c4d45042471be2a877d5e0e2290d7f
jhr <jhr@jhr-HP>
2017-05-08 19:01:47 +0900

    mybatis 연동 진행중

---
+++ restful/src/main/java/com/roh/restful/board/mapper/TestMapper.java
@@ -0,0 +1,8 @@
+package com.roh.restful.board.mapper;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public interface TestMapper {
+	public ArrayList<HashMap<String, String>> getGcwApproval();
+}
---
+++ restful/src/main/java/com/roh/restful/config/MybatisConfig.java
@@ -0,0 +1,27 @@
+package com.roh.restful.config;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.tomcat.jdbc.pool.DataSource;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+@Configuration
+@MapperScan(value={"com.roh.restful.**.mapper"})
+public class MybatisConfig {
+	@Bean
+	public SqlSessionFactory sqlSessionFactory(DataSource datasource) throws Exception {
+		SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+		sqlSessionFactory.setDataSource(datasource);
+		sqlSessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver().getResources("classpath:mapper/**/*.xml"));
+		return (SqlSessionFactory) sqlSessionFactory.getObject();
+	}
+	
+	@Bean
+	public SqlSessionTemplate sqlSession(SqlSessionFactory sqlSessionFactory) {
+		return new SqlSessionTemplate(sqlSessionFactory);
+	}
+}
