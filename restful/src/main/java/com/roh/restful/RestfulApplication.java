1de68678e7987fae4ad1f52989bb231956c9d2c3
JHR-GRAM <JHR-GRAM@DESKTOP-HEQRILU>
2017-06-20 17:30:33 +0900

    1. oauth2 서버 연동 2. JWT 사용

--- restful/src/main/java/com/roh/restful/RestfulApplication.java
+++ restful/src/main/java/com/roh/restful/RestfulApplication.java
@@ -1,35 +1,15 @@
 package com.roh.restful;
 
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.tomcat.jdbc.pool.DataSource;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-import com.roh.restful.board.mapper.TestMapper;
 
-
-@EnableResourceServer
 @SpringBootApplication
+@EnableResourceServer
 public class RestfulApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulApplication.class, args);
-	}
-	
-	@Autowired
-	private DataSource dataSource;
-	
-	@Autowired
-	private SqlSessionFactory sqlSessionFactory;
-	
-	@Autowired
-	private TestMapper testMapper;
-	
-	@Bean
-	public void test() {
-		
 	}
 }
