defeb179c886ea7fb81b1b05b5482eb055fcdab8
JHR-GRAM <JHR-GRAM@DESKTOP-HEQRILU>
2017-06-26 16:24:58 +0900

    comment delete

--- oauth/src/main/java/com/roh/oauth/configs/SecurityConfig.java
+++ oauth/src/main/java/com/roh/oauth/configs/SecurityConfig.java
@@ -20,9 +20,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 import com.roh.oauth.service.CustomUserDetailsService;
 
